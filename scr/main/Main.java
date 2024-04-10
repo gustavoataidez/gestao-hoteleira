6d0969ab0aab9a659250c9c072adc69d1224fdc0
gustavoataidez <luisgustavoataidee@gmail.com>
2024-04-10 18:58:04 -0300

    Commit inicial

---
+++ scr/main/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Menu start = new Menu(); //Criando um objeto da classe Menu
+        start.initialize(); //Chamar método de inicialização
+        start.menu(); //Método de menu de chamada
+    }
+}
+
---
+++ scr/main/Room.java
@@ -0,0 +1,21 @@
+
+public class Room {
+
+    private String mainNome;
+
+    //Declaração do construtor
+    public Room() {
+        mainNome = "e";
+    }
+
+    //Definir método
+    public void setName(String aName) {
+        mainNome = aName;
+    }
+
+    //Obter método
+    public String getName() {
+        return mainNome;
+    }
+
+}
