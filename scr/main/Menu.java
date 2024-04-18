3a1c1e441920061d446e1e84f6ec8fb702a6299e
Adenilson Silva <adenilsonsantos8891@gmail.com>
2024-04-17 21:59:01 -0300

    'feat: Inclusão da classe hotel'

--- scr/main/Menu.java
+++ scr/main/Menu.java
@@ -8,45 +8,49 @@ public class Menu {
 
     Room[] meuHotel = new Room[11];
 
-    static String escolha;
+    static int escolha;
     static String nomeQuarto;
     static int numQuarto = 1;
     static String resposta;
 
     Queue queueObj = new Queue(); //Criando um objeto da classe Queue
 
+    Hotel hotel = new Hotel();
+
     public void menu() {
-        System.out.println("*          Sistema de Gerenciamento de Hoteis       *");
+        System.out.println("*          Sistema de Gerenciamento de Hoteis        *");
         System.out.println("-                                                    *");
-        System.out.println("* 1. Visualizar todos os hoteis                     *");
+        System.out.println("* 1. Visualizar todos os hoteis                      *");
         System.out.println("* 2. Sair do Programa                                *");
         System.out.println("-                                                    *");
         System.out.println("");
         System.out.println("Escolha uma das opções acima. (Ex: Digite '1' para visualizar todos os hoteis)");
 
-        do {
-            System.out.println();
+        
+    do {
+        System.out.println();
+        System.out.print("Escolha: ");
+        while (!input.hasNextInt()) { // Verifica se a entrada do usuário não é um número inteiro
+            System.out.println("Entrada inválida! Por favor, insira um valor válido.");
             System.out.print("Escolha: ");
-            escolha = input.next();
-            String selecao = escolha.toLowerCase(); 
-
-            switch (selecao) {
-
-                case "1":
-                    visualizarQuartos();
-                    break;
-                    
-                case "2":
-                    System.out.println("Obrigado!");
-                    break;
-
-                default:
-                    System.out.println("Entrada inválida! Por favor insira uma das opções a seguir: 1 ou 2");
-            }
+            input.next(); // Descarta a entrada inválida
         }
-        while (!(escolha.equalsIgnoreCase("1") || escolha.equalsIgnoreCase("2"))); 
-    }
+        escolha = input.nextInt();
 
+        switch (escolha) {
+            case 1:
+                /*visualizarQuartos();*/
+                hotel.menuHotel();
+                break;
+            case 2:
+                System.out.println("Obrigado!");
+                break;
+            default:
+                System.out.println("Entrada inválida! Por favor insira uma das opções a seguir: 1 ou 2");
+        }
+    } while (escolha != 1 && escolha != 2);
+
+}
     public void initialize() {
         for (int x = 1; x < 11; x++) {
             meuHotel[x] = new Room(); //Criando 10 objetos de ambiente
