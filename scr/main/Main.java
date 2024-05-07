fe5d75a164aa3ee74e6c2cfefa061490640d036d
gustavoataidez <luisgustavoataidee@gmail.com>
2024-05-07 13:46:28 -0300

    feat: conexão com o banco de dados

--- scr/main/Main.java
+++ scr/main/Main.java
@@ -1,9 +1,12 @@
 
 public class Main {
     public static void main(String[] args) {
-        Menu start = new Menu(); //Criando um objeto da classe Menu
+        //Testando a conexão com o banco de dados
+        Conexao.getConexao();
+
+        /* Menu start = new Menu(); //Criando um objeto da classe Menu
         start.initialize(); //Chamar método de inicialização
-        start.menu(); //Método de menu de chamada
+        start.menu(); //Método de menu de chamada */
     }
 }
 
