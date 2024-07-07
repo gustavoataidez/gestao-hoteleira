ace2662daeb821bd214e18344b7d2bc102b55012
Joberval Magalhães dos Santos <29413697+jobervalmagalhaes@users.noreply.github.com>
2024-07-06 23:27:23 -0300

    Adição do conceito de heranças
    Adição do arquivo 'BaseConexao.java' e edição do arquivo 'Conexao.java', para adicionar a herança.

---
+++ src/conexao/BaseConexao.java
@@ -0,0 +1,21 @@
+package conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class BaseConexao {
+    public static Connection getConexao() {
+        try {
+            Connection conn = DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/hoteis",
+                "postgres",
+                "1234"
+            );
+            System.out.println("Conectado com o banco.");
+            return conn;
+        } catch (Exception e) {
+            System.out.println("Erro ao se conectar com o banco: " + e.getMessage());
+            return null;
+        }
+    }
+}
