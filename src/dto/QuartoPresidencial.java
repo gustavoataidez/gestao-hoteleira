539fbca2004a779bb74cbb06bfdfaa624d6c11d7
Alan Farias <aaof.alan@gmail.com>
2024-07-24 12:49:21 -0300

    refactor: Quarto Presidencial

--- src/dto/QuartoPresidencial.java
+++ src/dto/QuartoPresidencial.java
@@ -2,12 +2,10 @@ package dto;
 
 public class QuartoPresidencial extends Quarto {
     private boolean temSalaDeEstar;
-    private boolean temPiscinaPrivativa;
 
-    public QuartoPresidencial(int codigo, int hotel, String nome, int camas, int valor_dia, boolean temSalaDeEstar, boolean temPiscinaPrivativa) {
-        super(codigo, hotel, nome, camas, valor_dia);
+    public QuartoPresidencial(int idHotel, String nome, int camas, int valorDia, boolean temSalaDeEstar, boolean jacuzzi) {
+        super(idHotel, nome, camas, valorDia, true, temSalaDeEstar);
         this.temSalaDeEstar = temSalaDeEstar;
-        this.temPiscinaPrivativa = temPiscinaPrivativa;
     }
 
     public boolean isTemSalaDeEstar() {
@@ -18,16 +16,10 @@ public class QuartoPresidencial extends Quarto {
         this.temSalaDeEstar = temSalaDeEstar;
     }
 
-    public boolean isTemPiscinaPrivativa() {
-        return temPiscinaPrivativa;
-    }
 
-    public void setTemPiscinaPrivativa(boolean temPiscinaPrivativa) {
-        this.temPiscinaPrivativa = temPiscinaPrivativa;
-    }
 
     @Override
     public String toString() {
-        return super.toString() + " | Presidencial: " + (temSalaDeEstar ? "Com Sala de Estar" : "Sem Sala de Estar") + " | " + (temPiscinaPrivativa ? "Com Piscina Privativa" : "Sem Piscina Privativa");
+        return super.toString() + " | Presidencial: " + (temSalaDeEstar ? "Com Sala de Estar" : "Sem Sala de Estar") + " | ";
     }
 }
