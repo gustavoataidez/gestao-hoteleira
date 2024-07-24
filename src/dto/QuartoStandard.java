82368b470b0c61d0cda8d00bba5a0f8bb3a93c5a
Alan Farias <aaof.alan@gmail.com>
2024-07-24 12:49:53 -0300

    refactor: Quarto Standard

--- src/dto/QuartoStandard.java
+++ src/dto/QuartoStandard.java
@@ -1,7 +1,7 @@
 package dto;
 
 public class QuartoStandard extends Quarto {
-    public QuartoStandard(int codigo, int hotel, String nome, int camas, int valor_dia) {
-        super(codigo, hotel, nome, camas, valor_dia);
+    public QuartoStandard(int hotel, String nome, int camas, int valor_dia) {
+        super(hotel, nome, camas, valor_dia, false, false);
     }
 }
